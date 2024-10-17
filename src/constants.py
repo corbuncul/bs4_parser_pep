e79673401de0d45e0c145610e71e614e6751cfd0
corbuncul <agafivan@gmai.com>
2024-10-18 02:36:23 +0300

    for review

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
 """Костанты парсера."""
+
 from pathlib import Path
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
@@ -10,8 +11,26 @@ PEP_DOC_URL = 'https://peps.python.org/'
 BASE_DIR = Path(__file__).parent
 """Рабочая директория."""
 
+DOWNLOAD_DIR = BASE_DIR / 'downloads'
+"""Директория для скачивания."""
+
+RESULTS_DIR = BASE_DIR / 'results'
+"""Директория с результатами парсинга."""
+
+LOG_DIR = BASE_DIR / 'logs'
+"""Директория с логами."""
+
+LOG_FILE = LOG_DIR / 'parser.log'
+"""Файл для логов."""
+
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+"""Формат вывода в логи."""
+
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+"""Формат даты/времени для логов"""
+
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
-"""Формат даты/времени для логов."""
+"""Формат даты/времени для файлов CSV."""
 
 EXPECTED_STATUS = {
     'A': ('Active', 'Accepted'),
@@ -24,3 +43,15 @@ EXPECTED_STATUS = {
     '': ('Draft', 'Active'),
 }
 """Статусы документов PEP."""
+
+PRETTY = 'pretty'
+"""Режим для вывода в таблицу на консоль."""
+
+FILE = 'file'
+"""Режим для вывода в файл."""
+
+DEFAULT = ''
+"""Режим по умолчанию. Простой вывод на консоль."""
+
+CHOICES = (PRETTY, FILE)
+"""Выбор из режимов."""
--- src/outputs.py
+++ src/outputs.py
@@ -1,32 +1,31 @@
 """Модуль вывода результатов."""
+
 import csv
 import datetime as dt
 import logging
 
 from prettytable import PrettyTable
 
-from constants import BASE_DIR, DATETIME_FORMAT
+from constants import (
+    DATETIME_FORMAT,
+    RESULTS_DIR,
+    PRETTY,
+    FILE,
+    DEFAULT,
+    CHOICES,
+)
 
 
-def control_output(results, cli_args):
-    """Вывод данных в консоль или файл."""
-    output = cli_args.output
-    if output == 'pretty':
-        pretty_output(results)
-    elif output == 'file':
-        file_output(results, cli_args)
-    else:
-        default_output(results)
-
-
-def default_output(results):
+def default_output(*args):
     """Простой вывод результатов."""
+    results = args[0]
     for row in results:
         print(*row)
 
 
-def pretty_output(results):
+def pretty_output(*args):
     """Вывод данных в консоль в виде таблицы."""
+    results = args[0]
     table = PrettyTable()
     table.field_names = results[0]
     table.align = 'l'
@@ -34,16 +33,31 @@ def pretty_output(results):
     print(table)
 
 
-def file_output(results, cli_args):
+def file_output(*args):
     """Вывод результатов в файл CSV."""
-    results_dir = BASE_DIR / 'results'
-    results_dir.mkdir(exist_ok=True)
-    parser_mode = cli_args.mode
+    results = args[0]
+    RESULTS_DIR.mkdir(exist_ok=True)
+    parser_mode = args[1].mode
     now = dt.datetime.now()
     now_formatted = now.strftime(DATETIME_FORMAT)
     file_name = f'{parser_mode}_{now_formatted}.csv'
-    file_path = results_dir / file_name
+    file_path = RESULTS_DIR / file_name
     with open(file_path, 'w', encoding='utf-8') as f:
         writer = csv.writer(f, dialect='unix')
         writer.writerows(results)
     logging.info('Файл с результатами был сохранён: %s', file_path)
+
+
+OUTPUT_FUNCTIONS = {
+    PRETTY: pretty_output,
+    FILE: file_output,
+    DEFAULT: default_output,
+}
+
+
+def control_output(results, cli_args):
+    """Вывод данных в консоль или файл."""
+    output = cli_args.output
+    if output not in CHOICES:
+        output = DEFAULT
+    OUTPUT_FUNCTIONS[output](results, cli_args)
